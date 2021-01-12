fdf00ac0ffcda4f125d40e67dab8d157f3fb0c28
Tatiane Fernanda Machado <tatiane.nandam@gmail.com>
2021-01-11 21:34:58 -0300

    comit inicial

---
+++ usuario.js
@@ -0,0 +1,12 @@
+
+ 
+
+var capturando = "";
+function capturar () {
+    capturando = document.getElementById('a').value;
+    document.getElementById('valorDigitado').innerHTML = capturando;
+    alert(capturando+ " "+"seu cadastro foi realizado com sucesso.");
+
+
+       
+}
